3397d4ef436c9fe76cbca78b5876771c1e03d8e0
ThePrimeDev <theprimemain@gmail.com>
2023-06-08 10:27:17 +0300

    asd

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [image(), tailwind()],
+  base: "/asd/peta-astro/",
+});
